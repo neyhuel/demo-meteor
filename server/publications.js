a4ce620b7018251f921a7d4648aee27c9a0fb15a
neyhuel <nahue.martinez1987@gmail.com>
2016-09-12 16:53:28 -0300

    Añadiendo comentarios

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
 Meteor.publish('posts', function() {
-  return Posts.find({ flagged: false });
+	return Posts.find({ flagged: false });
+});
+
+Meteor.publish('comments', function() {
+	return Comments.find();
 });
