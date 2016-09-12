3b6c638601b47ea529d71304b684947c937f5338
neyhuel <nahue.martinez1987@gmail.com>
2016-09-12 14:35:11 -0300

    Validando antes de editar

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,17 @@
+Template.postEdit.onCreated(function() {
+    Session.set('postEditErrors', {});
+});
+
+Template.postEdit.helpers({
+    errorMessage: function(field) {
+        return Session.get('postEditErrors')[field];
+    },
+    errorClass: function(field) {
+        return !!Session.get('postEditErrors')[field] ? 'has-error' :
+            '';
+    }
+});
+
 Template.postEdit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -9,6 +23,10 @@ Template.postEdit.events({
       title: $(e.target).find('[name=title]').val(),
       flagged: $(e.target).find('[name=flagged]').is(":checked")
     }
+
+    var errors = validatePost(postProperties);
+    if (errors.title || errors.url)
+        return Session.set('postEditErrors', errors);
 
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
       if (error) {
