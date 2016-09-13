c857a4193d666d847505e00875b12a96a8466911
neyhuel <nahue.martinez1987@gmail.com>
2016-09-13 10:56:03 -0300

    Suscribiendonos a los comentarios al momento de entrar al post

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,7 @@ Meteor.publish('posts', function() {
 	return Posts.find({ flagged: false });
 });
 
-Meteor.publish('comments', function() {
-	return Comments.find();
+Meteor.publish('comments', function(postId) {
+	check(postId, String);
+	return Comments.find({postId: postId});
 });
